a85c191cf84298dccb018cbe442451d0fcec072d
waleedahmedwaheed <waleedahmedwaheed@gmail.com>
2023-07-04 21:30:30 +0500

    ccc

---
+++ precache-manifest.aa7e626e00df3f1b8106112e0000818e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "599d6896e04e7b06d6e9",
+    "url": "/static/css/main.d96c3eb6.chunk.css"
+  },
+  {
+    "revision": "599d6896e04e7b06d6e9",
+    "url": "/static/js/main.37461a72.chunk.js"
+  },
+  {
+    "revision": "ff49df8bb83d13228e07",
+    "url": "/static/js/runtime~main.fb0988b1.js"
+  },
+  {
+    "revision": "b8d3452a3ceed2ddccfd",
+    "url": "/static/css/2.a2dfd887.chunk.css"
+  },
+  {
+    "revision": "b8d3452a3ceed2ddccfd",
+    "url": "/static/js/2.b014f1ad.chunk.js"
+  },
+  {
+    "revision": "36bde6f0914f99b07518",
+    "url": "/static/js/3.7cca2875.chunk.js"
+  },
+  {
+    "revision": "86222edbdcf02d796e8f73b88e0ee351",
+    "url": "/index.html"
+  }
+];
